d2d0a298ee92a2607691a405b678a7f4a41154e7
BoBeenLee <cultist_tp@naver.com>
2019-02-06 21:07:37 +0900

    코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import _ from "lodash";
 import { Navigation } from "react-native-navigation";
+import firebase from "react-native-firebase";
 
 import RootStore from "./stores/RootStore";
-import { start as startNavigator } from "./utils/navigator";
+import { start as startNavigator, navigateDynamicLink } from "./utils/navigator";
 import { setup } from "../ReactotronConfig";
 import { registerScreens } from "./screens";
 
 const rootStore = RootStore.create();
 
 setup(rootStore);
-
 registerScreens(rootStore);
 
-function start() {
+firebase.links().onLink(url => {
+  console.log("onLink", url);
+});
+
+async function start() {
+  firebase.links().getInitialLink().then(url => {
+    console.log("start", url);
+  });
+  // if (url) {
+  //   Navigation.events().registerAppLaunchedListener(() => {
+  //     navigateDynamicLink();
+  //   });
+  // } else {
   Navigation.events().registerAppLaunchedListener(() => {
     startNavigator();
   });
+  // }
 }
 
 start();
--- src/screens/DynamicLinkScreen.tsx
+++ src/screens/DynamicLinkScreen.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from "styled-components/native";
+import firebase from 'react-native-firebase';
 
 import { GButton, ContainerWithStatusBar } from "../components";
 
@@ -17,7 +18,21 @@ class DynamicLinkScreen extends Component {
     }
 
     private shareLink = () => {
-        // NOTHING
+        const link = new firebase.links.DynamicLink('https://cosmee.co/', 'goalnexters.page.link')
+            .android.setPackageName('com.nexters.goal')
+            .ios.setBundleId('com.nexters.goal')
+            .analytics.setCampaign("facebook")
+            .analytics.setContent("cosmee ad")
+            .analytics.setSource("https://cosmee.co/")
+            .social.setImageUrl("https://cosmee.co/images/meta-image.png")
+            .social.setTitle("facebook social ad")
+            .social.setDescriptionText("hello cosmee");
+
+        firebase.links()
+            .createShortDynamicLink(link, 'UNGUESSABLE')
+            .then((url) => {
+                console.log(url);
+            });
     }
 }
 
